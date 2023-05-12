2a895ddb567bbf21ee4c11483141dd748fc175ef
kkinastowski <kkinastowski@fluence.pl>
2023-05-12 14:02:17 +0200

    some fun wit openai.Image and openai.Completion

--- openai_learning/__main__.py
+++ openai_learning/__main__.py
@@ -1,5 +1,24 @@
 import openai
 from UserCredentials import openai_api_key
+import PIL
 
 openai.api_key = openai_api_key
 models_list = openai.Model.list()
+
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="who is barrack obama",
+  max_tokens=50,
+  temperature=0.2
+)
+
+
+print(response)
+
+description = "cat shooting laser"
+
+image_resp = openai.Image.create(prompt=description, n=4, size="512x512")
+image_url = image_resp['data'][0]['url']
+
+print(image_url)
